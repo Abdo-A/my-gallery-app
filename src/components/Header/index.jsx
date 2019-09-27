ccd6bd885da0a9ca3bce2a30fe50e9cba9c8a03e
Abdo <abdopoetry@gmail.com>
2019-09-27 14:47:33 +0200

    Build basic post modal

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'antd';
+import PostModal from '../PostModal';
 
-const Header = () => <h3>Header</h3>;
+const Header = () => {
+  const [postModalVisible, setPostModalVisible] = useState(false);
+
+  const openPostModal = () => {
+    setPostModalVisible(true);
+  };
+
+  const closePostModal = () => {
+    setPostModalVisible(false);
+  };
+
+  return (
+    <div>
+      <Button icon="picture" onClick={openPostModal}>Post a photo</Button>
+      <PostModal visible={postModalVisible} closeModal={closePostModal} />
+    </div>
+  );
+};
 
 export default Header;
--- src/components/PostModal/index.jsx
+++ src/components/PostModal/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  Modal, Upload, Button, Icon,
+} from 'antd';
 
-const PostModal = () => (
-  <h3>Post Modal</h3>
-);
+const PostModal = ({ visible, closeModal }) => {
+  const handleFileUpload = (info) => {
+    console.log(info.file);
+  };
 
+  return (
+    <Modal
+      title="Post a photo"
+      visible={visible}
+      closable={false}
+      onOk={closeModal}
+      onCancel={closeModal}
+    >
+      <Upload beforeUpload={() => false} onChange={handleFileUpload} showUploadList={false}>
+        <Button>
+          <Icon type="upload" />
+          {' '}
+          Click to Upload
+        </Button>
+      </Upload>
+    </Modal>
+  );
+};
 export default PostModal;
